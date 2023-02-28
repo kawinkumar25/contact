11038de35e99f4693885f048b8ae206d449c1c41
kawinkumar25 <123533543+kawinkumar25@users.noreply.github.com>
2023-02-28 14:14:36 +0530

    Add files via upload

---
+++ src/main/java/org/example/Details.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Details {
+    String name;
+    String phone;
+    String email;
+    Details(String name,String phone,String email)
+    {
+        this.name=name;
+        this.phone=phone;
+        this.email=email;
+    }
+}
